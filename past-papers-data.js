e4c32b01f45211789e9125a97c7ab23b0cb80fe0
manazarali01-hub <manazarali01@gmail.com>
2026-09-18 18:53:05 +0500

    Add Pakistan SSC HSSC board catalog

---
+++ past-papers-data.js
@@ -0,0 +1,46 @@
+window.EDUNIZAM_PAST_PAPERS = {
+  updatedAt: "2026-09-18",
+  boards: [
+    {id:"fbise",name:"FBISE Islamabad",region:"Federal / GB sub-offices",classes:[9,10,11,12],archiveUrl:"https://pakboards.com/past-papers/fbise",officialUrl:"https://fbise.edu.pk/"},
+    {id:"bise-lahore",name:"BISE Lahore",region:"Punjab",classes:[9,10,11,12],archiveUrl:"https://pakboards.com/past-papers/bise-lahore",officialUrl:"https://www.biselahore.com/"},
+    {id:"bise-gujranwala",name:"BISE Gujranwala",region:"Punjab",classes:[9,10,11,12],officialUrl:"https://www.bisegrw.edu.pk/"},
+    {id:"bise-faisalabad",name:"BISE Faisalabad",region:"Punjab",classes:[9,10,11,12],archiveUrl:"https://pakboards.com/past-papers/bise-faisalabad",officialUrl:"https://www.bisefsd.edu.pk/"},
+    {id:"bise-multan",name:"BISE Multan",region:"Punjab",classes:[9,10,11,12],archiveUrl:"https://pakboards.com/past-papers/bise-multan",officialUrl:"https://www.bisemultan.edu.pk/"},
+    {id:"bise-rawalpindi",name:"BISE Rawalpindi",region:"Punjab",classes:[9,10,11,12],archiveUrl:"https://pakboards.com/past-papers/bise-rawalpindi",officialUrl:"https://www.biserawalpindi.edu.pk/"},
+    {id:"bise-sargodha",name:"BISE Sargodha",region:"Punjab",classes:[9,10,11,12],officialUrl:"https://www.bisesargodha.edu.pk/"},
+    {id:"bise-bahawalpur",name:"BISE Bahawalpur",region:"Punjab",classes:[9,10,11,12],officialUrl:"https://bisebwp.edu.pk/"},
+    {id:"bise-dgkhan",name:"BISE D.G. Khan",region:"Punjab",classes:[9,10,11,12],officialUrl:"https://www.bisedgkhan.edu.pk/"},
+    {id:"bise-sahiwal",name:"BISE Sahiwal",region:"Punjab",classes:[9,10,11,12],officialUrl:"https://bisesahiwal.edu.pk/"},
+    {id:"bsek-karachi",name:"BSEK Karachi",region:"Sindh",classes:[9,10],officialUrl:"https://www.bsek.edu.pk/"},
+    {id:"biek-karachi",name:"BIEK Karachi",region:"Sindh",classes:[11,12],officialUrl:"https://www.biek.edu.pk/"},
+    {id:"bise-hyderabad",name:"BISE Hyderabad",region:"Sindh",classes:[9,10,11,12],officialUrl:"https://www.biseh.edu.pk/"},
+    {id:"bise-sukkur",name:"BISE Sukkur",region:"Sindh",classes:[9,10,11,12],officialUrl:"https://www.bisesuksindh.edu.pk/"},
+    {id:"bise-larkana",name:"BISE Larkana",region:"Sindh",classes:[9,10,11,12],officialUrl:"https://www.biselrk.edu.pk/"},
+    {id:"bise-mirpurkhas",name:"BISE Mirpurkhas",region:"Sindh",classes:[9,10,11,12],officialUrl:"https://www.bisemirpurkhas.com/"},
+    {id:"bise-shaheed-benazirabad",name:"BISE Shaheed Benazirabad (Nawabshah)",region:"Sindh",classes:[9,10,11,12],officialUrl:"https://bisesba.edu.pk/"},
+    {id:"bise-peshawar",name:"BISE Peshawar",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],archiveUrl:"https://pakboards.com/past-papers/bise-peshawar",officialUrl:"https://www.bisep.edu.pk/"},
+    {id:"bise-abbottabad",name:"BISE Abbottabad",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://www.biseatd.edu.pk/"},
+    {id:"bise-bannu",name:"BISE Bannu",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://www.biseb.edu.pk/"},
+    {id:"bise-dikhan",name:"BISE D.I. Khan",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://www.bisedik.edu.pk/"},
+    {id:"bise-kohat",name:"BISE Kohat",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://www.bisekt.edu.pk/"},
+    {id:"bise-malakand",name:"BISE Malakand",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://www.bisemalakand.edu.pk/"},
+    {id:"bise-mardan",name:"BISE Mardan",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://web.bisemdn.edu.pk/"},
+    {id:"bise-swat",name:"BISE Swat",region:"Khyber Pakhtunkhwa",classes:[9,10,11,12],officialUrl:"https://www.bisess.edu.pk/"},
+    {id:"bbise-quetta",name:"BBISE Quetta",region:"Balochistan",classes:[9,10,11,12],officialUrl:"https://bbiseqta.edu.pk/"},
+    {id:"bise-ajk",name:"BISE AJK Mirpur",region:"Azad Jammu & Kashmir",classes:[9,10,11,12],officialUrl:"https://ajkbise.net/"},
+    {id:"aku-eb",name:"Aga Khan University Examination Board (AKU-EB)",region:"National / Private",classes:[9,10,11,12],archiveUrl:"https://examinationboard.aku.edu/learning-materials/Pages/home.aspx",officialUrl:"https://examinationboard.aku.edu/"}
+  ],
+  groups: {
+    "9":["Science","Arts / Humanities","General / Technical"],
+    "10":["Science","Arts / Humanities","General / Technical"],
+    "11":["Pre-Medical","Pre-Engineering","ICS","ICom","FA / Humanities","General Science","Technical"],
+    "12":["Pre-Medical","Pre-Engineering","ICS","ICom","FA / Humanities","General Science","Technical"]
+  },
+  subjects: {
+    "9":["English","Urdu","Islamiat / Ethics","Pakistan Studies","Mathematics","Physics","Chemistry","Biology","Computer Science","General Science","Regional Language"],
+    "10":["English","Urdu","Islamiat / Ethics","Pakistan Studies","Mathematics","Physics","Chemistry","Biology","Computer Science","General Science","Regional Language"],
+    "11":["English","Urdu","Islamiat / Ethics","Mathematics","Physics","Chemistry","Biology","Computer Science","Statistics","Economics","Accounting","Education","Civics","Psychology","Sociology"],
+    "12":["English","Urdu","Pakistan Studies","Islamiat / Ethics","Mathematics","Physics","Chemistry","Biology","Computer Science","Statistics","Economics","Accounting","Education","Civics","Psychology","Sociology"]
+  },
+  years:[2025,2024,2023,2022,2021,2020,2019,2018,2017,2016,2015]
+};
