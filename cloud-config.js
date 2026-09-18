7a5c4a0eb9c4f5fe2b8db0e3d13ed6743089a02e
manazarali01-hub <manazarali01@gmail.com>
2026-09-18 20:57:52 +0500

    Add safe disabled cloud config

---
+++ cloud-config.js
@@ -0,0 +1,9 @@
+window.EDUNIZAM_CLOUD_CONFIG = window.EDUNIZAM_CLOUD_CONFIG || {
+  enabled: false,
+  provider: "supabase",
+  supabaseUrl: "",
+  supabasePublishableKey: "",
+  institutionId: "",
+  admissionsStorageBucket: "admission-documents",
+  paymentApiBaseUrl: ""
+};
