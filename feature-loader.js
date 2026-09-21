631032fd0307e16c377eec7d99105bda83f3edab
manazarali01-hub <manazarali01@gmail.com>
2026-09-21 21:17:39 +0500

    Refresh branded EduNizam feature assets

--- feature-loader.js
+++ feature-loader.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='20260921-access62';
+  const VERSION='20260921-brand66';
   const featureScripts={
     attendanceanalytics:['attendance-analytics.js'],
     pastpapers:['past-papers-premium.js'],
